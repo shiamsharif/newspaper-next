7b7d0688b1701b7f2dee8d43cf5ede163a96a390
shiamsharif <shiam.sharif.07@gmail.com>
2026-05-31 11:24:06 +0600

    add header page

--- src/components/Header/page.jsx
+++ src/components/Header/page.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import logo from "@/assets/logo.png";
+import Image from 'next/image';
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div className='text-center py-8'>
+            <Image src={logo} width={300} height={200} alt="Logo" className='mx-auto' />
+            <p>News Without fear or favour</p>
+            <p>{format(new Date(), "EEEE, MMM dd, yyyy")}</p>
         </div>
     );
 };
